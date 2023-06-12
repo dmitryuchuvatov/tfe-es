33556329b696ebfacdda0a6e55049824ef9d1a19
Dmitry Uchuvatov <dmitry.uchuvatov@hashicorp.com>
2023-06-12 10:44:31 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,3 @@
-output "public_ip" {
-  value = aws_eip.eip_tfe.public_ip
-}
-
-output "ssh_login" {
-  description = "SSH login command"
-  value       = "ssh -i tfesshkey.pem ubuntu@${local.fqdn}"
-}
-
 output "replicated_dashboard" {
   description = "URL for Replicated dashboard"
   value       = "https://${local.fqdn}:8800"
@@ -17,7 +8,7 @@ output "tfe_login" {
   value       = "https://${local.fqdn}"
 }
 
-output "release_sequence" {
-  description = "Installed release number of TFE"
-  value       = var.release_sequence
+output "ssh_login" {
+  description = "SSH login command"
+  value       = "ssh -i tfesshkey.pem ubuntu@${local.fqdn}"
 }
